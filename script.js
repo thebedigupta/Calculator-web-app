9684fb476df38bab67290a7cc40da810e04535fb
Bedi Gupta <Bedigupta76@gmail.com>
2023-09-30 18:47:36 +0530

    Basic calculator web app

---
+++ script.js
@@ -0,0 +1,101 @@
+let displayValue = ""
+
+function appendToDisplay(value){
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+
+function calculateResult(){
+    displayValue = eval(displayValue);
+    document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay(){
+    displayValue = ""
+    document.getElementById('display').value = displayValue;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let displayValue = "";
+
+// function appendToDisplay(value) {
+//     displayValue += value;
+//     document.getElementById("display").value = displayValue;
+// }
+
+// function clearDisplay() {
+//     displayValue = "";
+//     document.getElementById("display").value = displayValue;
+// }
+
+// function calculateResult() {
+//     try {
+//         displayValue = eval(displayValue);
+//         document.getElementById("display").value = displayValue;
+//     } catch (error) {
+//         document.getElementById("display").value = "Error";
+//     }
+// }
